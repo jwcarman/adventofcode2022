596b5969dba34b65329d56e36e939aa80156c58a
James Carman <jcarman@callibrity.com>
2022-12-01 17:41:43 -0500

    adding copyright.

--- src/test/kotlin/adventofcode/Input.kt
+++ src/test/kotlin/adventofcode/Input.kt
@@ -1,7 +1,27 @@
+/*
+ * Copyright (c) ${date.year} James Carman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package adventofcode
 
 import java.io.File
 
 object Input {
-    fun readAsLines(name: String) = File("src/test/resources/${name}").readLines()
+    fun readAsLines(name: String) = File(resourceName(name)).readLines()
+
+    private fun resourceName(name: String) = "src/test/resources/${name}"
+
+    fun readAsString(name: String) = File(resourceName(name)).readText();
 }
