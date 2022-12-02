8aa8073b169ac499ecd27125992ec8058b6f96e9
James Carman <jcarman@callibrity.com>
2022-12-02 08:43:01 -0500

    Day 2 complete.

--- src/test/kotlin/adventofcode/Day02.kt
+++ src/test/kotlin/adventofcode/Day02.kt
@@ -19,13 +19,39 @@ package adventofcode
 import org.junit.Test
 
 class Day02 {
+    private val part1Scores = mapOf(
+        "A X" to 4,
+        "A Y" to 8,
+        "A Z" to 3,
+        "B X" to 1,
+        "B Y" to 5,
+        "B Z" to 9,
+        "C X" to 7,
+        "C Y" to 2,
+        "C Z" to 6
+    )
+
+    private val part2Scores = mapOf(
+        "A X" to 3,
+        "A Y" to 4,
+        "A Z" to 8,
+        "B X" to 1,
+        "B Y" to 5,
+        "B Z" to 9,
+        "C X" to 2,
+        "C Y" to 6,
+        "C Z" to 7
+    )
+
     @Test
     fun part1() {
         val lines = Input.readAsLines("day02.txt")
+        println(lines.sumOf { it -> part1Scores.getOrDefault(it, 0) })
     }
 
     @Test
     fun part2() {
-        val lines = Input.readAsLines("day01.txt")
+        val lines = Input.readAsLines("day02.txt")
+        println(lines.sumOf { it -> part2Scores.getOrDefault(it, 0) })
     }
 }
